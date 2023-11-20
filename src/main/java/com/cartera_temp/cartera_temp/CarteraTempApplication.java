e985692f693b248da7ee4b9706122a771b7f7d13
Duvan Diaz <dudicodiaz@gmail.com>
2023-11-20 16:18:32 -0500

    c

---
+++ src/main/java/com/cartera_temp/cartera_temp/CarteraTempApplication.java
@@ -0,0 +1,13 @@
+package com.cartera_temp.cartera_temp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteraTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteraTempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartera_temp/cartera_temp/CarteraTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartera_temp.cartera_temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteraTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
