36e3e5093f8e8c4d1ca0214966408495185f8ba4
MigueBetoG <mabetancur70@misena.edu.co>
2023-11-29 08:51:14 -0500

    .

--- src/main/java/com/cartera_temp/cartera_temp/Controllers/GestionesController.java
+++ src/main/java/com/cartera_temp/cartera_temp/Controllers/GestionesController.java
@@ -3,9 +3,12 @@ package com.cartera_temp.cartera_temp.Controllers;
 import com.cartera_temp.cartera_temp.Dtos.GestionesDto;
 import com.cartera_temp.cartera_temp.Models.Gestiones;
 import com.cartera_temp.cartera_temp.Service.GestionesService;
+import java.util.List;
 import java.util.Objects;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,12 @@ public class GestionesController {
         if(Objects.isNull(gestion)){
             return ResponseEntity.badRequest().build();
         }
+        return ResponseEntity.ok(gestion);
+    }
+    
+    @GetMapping("/getGestionByNumObligacion/{num_obligacion}")
+    public ResponseEntity<List<Gestiones>> getGestionesByNumObligacion(@PathVariable("num_obligacion") String numeroObligacion){
+        List<Gestiones> gestion = gestionesService.findHistoricoGestiones(numeroObligacion);
         return ResponseEntity.ok(gestion);
     }
     
