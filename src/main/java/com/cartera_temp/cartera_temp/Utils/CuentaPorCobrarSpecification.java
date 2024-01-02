c55909b9bd08e45c68d8ccff6687de4a86c0b71a
Duvan Diaz <dudicodiaz@gmail.com>
2024-01-02 16:08:51 -0500

    c

--- src/main/java/com/cartera_temp/cartera_temp/Utils/CuentaPorCobrarSpecification.java
+++ src/main/java/com/cartera_temp/cartera_temp/Utils/CuentaPorCobrarSpecification.java
@@ -8,11 +8,14 @@ import com.cartera_temp.cartera_temp.Models.Gestiones;
 import com.cartera_temp.cartera_temp.ModelsClients.Usuario;
 import com.cartera_temp.cartera_temp.Service.UsuarioClientService;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
 import org.springframework.data.jpa.domain.Specification;
 
 public class CuentaPorCobrarSpecification {
@@ -27,8 +30,8 @@ public class CuentaPorCobrarSpecification {
                 predicates.add(root.get("banco").get("banco").in(filtro.getBanco()));
                 System.out.println("BANCO");
             }
-            
-            if(filtro.getSede() != null && !filtro.getSede().isEmpty()){
+
+            if (filtro.getSede() != null && !filtro.getSede().isEmpty()) {
                 predicates.add(root.get("sede").get("sede").in(filtro.getSede()));
                 System.out.println("SEDE");
             }
@@ -49,37 +52,35 @@ public class CuentaPorCobrarSpecification {
             }
 
             if (filtro.getFechaCompromisoInicio() != null && filtro.getFechaCompromisoFin() != null) {
+                Join<CuentasPorCobrar, Gestiones> gestionJoin = root.join("gestiones", JoinType.LEFT);
+                Join<Gestiones, ClasificacionGestion> clasificacionGestionJoin = gestionJoin.join("clasificacionGestion", JoinType.LEFT);
 
-                Join<CuentasPorCobrar, Gestiones> gJoin = root.join("gestiones", JoinType.LEFT);
-                Join<Gestiones, ClasificacionGestion> cgJoin = gJoin.join("clasificacionGestion", JoinType.LEFT);
-                Join<ClasificacionGestion, AcuerdoPago> cpJoin = cgJoin.join("acuerdoPago", JoinType.LEFT);
-                
-                predicates.add(criteriaBuilder.between(cpJoin.get("fechaCompromiso"), filtro.getFechaCompromisoInicio(), filtro.getFechaCompromisoFin()));
-                System.out.println("FECHA COMPROMISO");
-                
+                predicates.add(criteriaBuilder.between(
+                        criteriaBuilder.treat(clasificacionGestionJoin, AcuerdoPago.class).get("fechaCompromiso"),
+                        filtro.getFechaCompromisoInicio(), filtro.getFechaCompromisoFin()));
             }
-            
-            if(filtro.getFechaCpcInicio() != null && filtro.getFechaCpcFin() != null){
+
+            if (filtro.getFechaCpcInicio() != null && filtro.getFechaCpcFin() != null) {
                 predicates.add(criteriaBuilder.between(root.get("fechaCuentaCobrar"), filtro.getFechaCpcInicio(), filtro.getFechaCpcFin()));
                 System.out.println("FECHA CPC");
             }
-            
-            if(filtro.getFechaGestionInicio() != null && filtro.getFechaGestionFin() != null){
+
+            if (filtro.getFechaGestionInicio() != null && filtro.getFechaGestionFin() != null) {
                 predicates.add(criteriaBuilder.between(root.get("gestiones").get("fechaGestion"), filtro.getFechaGestionInicio(), filtro.getFechaGestionFin()));
                 System.out.println("GESTION");
             }
-            
-            if(filtro.getSaldoCapitalInicio() != null && filtro.getSaldoCapitalFin() != null){
+
+            if (filtro.getSaldoCapitalInicio() != null && filtro.getSaldoCapitalFin() != null) {
                 predicates.add(criteriaBuilder.between(root.get("totalObligatoria"), filtro.getSaldoCapitalInicio(), filtro.getSaldoCapitalFin()));
                 System.out.println("CAPITAL");
             }
-            
+
             predicates.add(criteriaBuilder.equal(root.get("asesor").get("usuarioId"), idUsuario));
-            
+
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
 
         };
-        
+
     }
 
 }
