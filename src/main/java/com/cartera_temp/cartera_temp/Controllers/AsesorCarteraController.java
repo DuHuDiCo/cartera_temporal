d797147d8ef409824ae394cd4256b8282466cfb4
MigueBetoG <mabetancur70@misena.edu.co>
2024-01-11 13:59:06 -0500

    Create AsesorCarteraController.java

---
+++ src/main/java/com/cartera_temp/cartera_temp/Controllers/AsesorCarteraController.java
@@ -0,0 +1,32 @@
+package com.cartera_temp.cartera_temp.Controllers;
+
+import com.cartera_temp.cartera_temp.Models.AsesorCartera;
+import com.cartera_temp.cartera_temp.Service.AsesorCarteraService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/asesorCartera")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class AsesorCarteraController {
+    
+    private final AsesorCarteraService acs;
+
+    public AsesorCarteraController(AsesorCarteraService acs) {
+        this.acs = acs;
+    }
+    
+    @GetMapping("/getAllAsesores")
+    public ResponseEntity<List<AsesorCartera>> getAllASesores(){
+        List<AsesorCartera> asesor = acs.listarAsesores();
+        return ResponseEntity.ok(asesor );
+    }
+    
+}
