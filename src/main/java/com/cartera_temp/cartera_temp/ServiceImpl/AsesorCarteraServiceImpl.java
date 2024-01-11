e4d41b1ac432b123154c3911129a25517c566380
MigueBetoG <mabetancur70@misena.edu.co>
2024-01-11 14:11:30 -0500

    Update AsesorCarteraServiceImpl.java

--- src/main/java/com/cartera_temp/cartera_temp/ServiceImpl/AsesorCarteraServiceImpl.java
+++ src/main/java/com/cartera_temp/cartera_temp/ServiceImpl/AsesorCarteraServiceImpl.java
@@ -1,11 +1,15 @@
 
 package com.cartera_temp.cartera_temp.ServiceImpl;
 
+import com.cartera_temp.cartera_temp.Dtos.AsesorCarteraResponse;
+import com.cartera_temp.cartera_temp.FeignClients.usuario_client;
 import com.cartera_temp.cartera_temp.Models.AsesorCartera;
+import com.cartera_temp.cartera_temp.ModelsClients.Usuario;
 import com.cartera_temp.cartera_temp.Service.AsesorCarteraService;
 import com.cartera_temp.cartera_temp.repository.AsesorCarteraRepository;
 import java.util.List;
 import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,12 +17,14 @@ public class AsesorCarteraServiceImpl implements AsesorCarteraService{
     
     
     private final AsesorCarteraRepository asesorCarteraRepository;
+    private final usuario_client usuClient;
+    private final HttpServletRequest request;
 
-    public AsesorCarteraServiceImpl(AsesorCarteraRepository asesorCarteraRepository) {
+    public AsesorCarteraServiceImpl(AsesorCarteraRepository asesorCarteraRepository, usuario_client usuClient, HttpServletRequest request) {
         this.asesorCarteraRepository = asesorCarteraRepository;
+        this.usuClient = usuClient;
+        this.request = request;
     }
-    
-    
 
     @Override
     public AsesorCartera guardarAsesor(Long asesor) {
@@ -35,6 +41,13 @@ public class AsesorCarteraServiceImpl implements AsesorCarteraService{
     @Override
     public List<AsesorCartera> listarAsesores() {
         List<AsesorCartera> asesor = asesorCarteraRepository.findAll();
+        AsesorCarteraResponse response = new AsesorCarteraResponse();
+        String token = request.getAttribute("token").toString();
+        for (AsesorCartera asesorCartera : asesor) {
+            
+            Usuario usu = usuClient.getUsuarioById(asesorCartera.getIdAsesorCartera(), token);
+            
+        }
         return asesor;
     }
 
