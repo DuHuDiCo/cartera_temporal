d9e3d0d37585f9b14f41f648c5044aed9ca3fb4c
Duvan Diaz <dudicodiaz@gmail.com>
2024-03-26 16:42:12 -0500

    c

--- src/main/java/com/cartera_temp/cartera_temp/Utils/CuentaPorCobrarSpecification.java
+++ src/main/java/com/cartera_temp/cartera_temp/Utils/CuentaPorCobrarSpecification.java
@@ -60,7 +60,7 @@ public class CuentaPorCobrarSpecification {
                 Join<Gestiones, Nota> notaJoin = criteriaBuilder.treat(clasificacionGestionJoin, Nota.class);
 //                Join<Gestiones, Tarea> tareaJoin = criteriaBuilder.treat(clasificacionGestionJoin, Tarea.class);
 //                predicates.add(criteriaBuilder.isTrue(tareaJoin.get("isActive")));
-                predicates.add(criteriaBuilder.equal(notaJoin.get("nombresClasificacion").get("idNombreClasificacion"), filtro.getClasificacionGestion().getId()));
+                predicates.add(notaJoin.get("nombresClasificacion").get("idNombreClasificacion").in(filtro.getClasificacionGestion().getId()));
 //                if (filtro.getClasificacionGestion().getTipoClasificacion().equals(TipoClasificacion.ACUERDODEPAGO.getDato())) {
 //                    System.out.println("ACUERDO");
 //                    System.out.println(filtro.getClasificacionGestion().getNombreClasificacion());
