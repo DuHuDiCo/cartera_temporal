4c4e746fd2f476760e0d51237045984386f2ff6a
Duvan Diaz <dudicodiaz@gmail.com>
2024-03-27 10:46:01 -0500

    c

--- src/main/java/com/cartera_temp/cartera_temp/Utils/Functions.java
+++ src/main/java/com/cartera_temp/cartera_temp/Utils/Functions.java
@@ -178,7 +178,7 @@ public class Functions {
 
     public static Date fechaDateToStringNormal(String fecha) throws ParseException {
 
-        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String date = formato.format(fecha);
 
         Date fechaOk = formato.parse(date);
