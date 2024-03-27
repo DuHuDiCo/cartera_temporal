7e448b6c13fb39b00a9f68a878f26a314e42f762
Duvan Diaz <dudicodiaz@gmail.com>
2024-03-27 11:58:17 -0500

    c

--- src/main/java/com/cartera_temp/cartera_temp/Utils/CuentaPorCobrarSpecification.java
+++ src/main/java/com/cartera_temp/cartera_temp/Utils/CuentaPorCobrarSpecification.java
@@ -72,7 +72,7 @@ public class CuentaPorCobrarSpecification {
                         predicates.add(criteriaBuilder.and(
                                 criteriaBuilder.equal(nombresClasificacionJoin.get("idNombreClasificacion"), filtro.getClasificacionGestion().getId()),
                                 criteriaBuilder.greaterThan(gestionesJoin.get("fechaGestion"), fecha),
-                                criteriaBuilder.equal(acuerdoPagoJoin.get("isActive"), true)
+                                criteriaBuilder.equal(acuerdoPagoJoin.get("isActive"), 1)
                         ));
                     } catch (ParseException ex) {
                         Logger.getLogger(CuentaPorCobrarSpecification.class.getName()).log(Level.SEVERE, null, ex);
