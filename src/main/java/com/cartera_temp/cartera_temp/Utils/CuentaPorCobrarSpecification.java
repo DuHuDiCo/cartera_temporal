5edc9a0a3524c0f8e5d2aecdf8c324eff0cda96e
Duvan Diaz <dudicodiaz@gmail.com>
2024-04-08 16:10:19 -0500

    c

--- src/main/java/com/cartera_temp/cartera_temp/Utils/CuentaPorCobrarSpecification.java
+++ src/main/java/com/cartera_temp/cartera_temp/Utils/CuentaPorCobrarSpecification.java
@@ -122,9 +122,10 @@ public class CuentaPorCobrarSpecification {
                         Join<Tarea, NombresClasificacion> nombresClasificacionJoin = notaJoin.join("nombresClasificacion");
 
                         predicates.add(criteriaBuilder.and(
+                                criteriaBuilder.between(gestionesJoin.get("fechaGestion"), Functions.fechaConHora(filtro.getFechaGestionInicio(), "inicio"), filtro.getFechaGestionFin()),
 //                                criteriaBuilder.equal(clasificacionGestionJoin.get("clasificacion"), filtro.getClasificacionGestion().getTipoClasificacion().toUpperCase()),
-                                criteriaBuilder.equal(nombresClasificacionJoin.get("idNombreClasificacion"), filtro.getClasificacionGestion().getId()),
-                                criteriaBuilder.between(gestionesJoin.get("fechaGestion"), Functions.fechaConHora(filtro.getFechaGestionInicio(), "inicio"), filtro.getFechaGestionFin())
+                                criteriaBuilder.equal(nombresClasificacionJoin.get("idNombreClasificacion"), filtro.getClasificacionGestion().getId())
+                                
                         ));
 
                         System.out.println(Functions.fechaConHora(filtro.getFechaGestionInicio(), "inicio").toString());
