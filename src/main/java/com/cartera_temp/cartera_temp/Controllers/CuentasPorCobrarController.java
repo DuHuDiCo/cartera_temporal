39d6a495357561be31c1b7811f90b7795f14a5f9
Duvan Diaz <dudicodiaz@gmail.com>
2024-09-16 14:40:54 -0500

    commit

--- src/main/java/com/cartera_temp/cartera_temp/Controllers/CuentasPorCobrarController.java
+++ src/main/java/com/cartera_temp/cartera_temp/Controllers/CuentasPorCobrarController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/v1/cuentas")
-@CrossOrigin(origins = { "*", "http://localhost:4000" }, maxAge = 3600)
+@CrossOrigin(origins = { "*", "http://localhost:4000", "http://localhost:4200" }, maxAge = 3600)
 public class CuentasPorCobrarController {
 
     private final CuentasPorCobrarService cuentasPorCobrarService;
